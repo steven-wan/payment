ff05e9a95cb608bdd2e74be341d72c7165f78cad
steven-wan <951520698@qq.com>
2019-10-14 15:35:17 +0800

    对外版本

--- payment-util/src/main/java/com/healthpay/healthPayUtil.java
+++ payment-util/src/main/java/com/healthpay/healthPayUtil.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.healthpay.dto.apidto.*;
 import com.healthpay.dto.httpbodydto.DataDTO;
 import com.healthpay.dto.httpbodydto.HeaderDTO;
-import com.healthpay.httpUtil.HttpClientUtil;
 import com.healthpay.securityUtil.SecurityUtil;
 import com.healthpay.dto.*;
 import org.apache.commons.lang3.StringUtils;
@@ -24,24 +23,6 @@ import java.util.regex.Pattern;
  * @date 2019-10-11 16:40
  */
 public class healthPayUtil {
-    /**
-     * 访问健保付聚合支付
-     *
-     * @param url         访问地址
-     * @param data        接口的data
-     * @param funcId      接口文档中的每个接口的代号
-     * @param privateKey  私钥
-     * @param developerId 开发者ID
-     * @param desKey      对称秘钥
-     * @return
-     * @throws Exception
-     */
-    public static String httpPostToJbfPay(String url, String data, String funcId, String privateKey, String developerId, String desKey) throws Exception {
-        String rquestBody = httpRquestBodyToJbfPay(data, funcId, privateKey, developerId, desKey);
-        String retXml = HttpClientUtil.httpPost(url, rquestBody, HttpClientUtil.CONTENT_TYPE);
-        String responseBody = httpResponseBodyFromJbfPay(retXml, null,desKey, funcId);
-        return responseBody;
-    }
 
     /**
      * 访问聚合支付请求报文组装
@@ -74,19 +55,20 @@ public class healthPayUtil {
 
     /**
      * 访问聚合支付返回报文解析
+     *
      * @param retXml
      * @param desKey
      * @param funcId
      * @return
      * @throws Exception
      */
-    public static String httpResponseBodyFromJbfPay(String retXml, String publicKey,String desKey, String funcId) throws Exception {
+    public static String httpResponseBodyFromJbfPay(String retXml, String publicKey, String desKey, String funcId) throws Exception {
         DataDTO retDataDto = JAXB.unmarshal(new StringReader(retXml), DataDTO.class);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("code", retDataDto.getHeader().getRetCode());
         jsonObject.put("msg", retDataDto.getHeader().getRetMsg());
         String retBody = SecurityUtil.decryptDes(retDataDto.getBody(), desKey, "utf-8");
-        if (!SecurityUtil.verifyRSA(retBody,publicKey,retDataDto.getHeader().getSignature(),SecurityUtil.CHARSET)) {
+        if (!SecurityUtil.verifyRSA(retBody, publicKey, retDataDto.getHeader().getSignature(), SecurityUtil.CHARSET)) {
             jsonObject.put("msg", "验签失败");
         } else {
             if ("0000".equals(retDataDto.getHeader().getRetCode()) && StringUtils.isNotBlank(retDataDto.getBody())) {
@@ -113,7 +95,7 @@ public class healthPayUtil {
                     case Constants.FUNC_ID_P7003:
                     case Constants.FUNC_ID_P7004:
                         jsonObject.put("data", retBody);
-                        return  jsonObject.toJSONString();
+                        return jsonObject.toJSONString();
                     default:
                         throw new Exception("错误的 funcId");
                 }
@@ -138,7 +120,7 @@ public class healthPayUtil {
     public static void main(String[] args) throws Exception {
         //http://172.16.104.91/gatewayOnline//gateway/portal/execute http://116.7.255.40:53501/gatewayOnline/gateway/portal/execute
         String url = "http://172.16.104.91/gatewayOnline//gateway/portal/execute";
-        
+
         String data = "<Data>" +
                 "<personal_id>20191008</personal_id>" +
                 "</Data>";
@@ -146,10 +128,11 @@ public class healthPayUtil {
         String desKey = "IIAa1quPRpg=";
         String developerId = "59027";
         String funcId = "F1006";
+        String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJj1ikbWnTAVsjiX0WL+E4qOqH8FL7kqpB5aNC+sm8bU6JssgNwwCuAKi0/+vMFfviKtcx6Za/6O9TXtxnBo9bECAwEAAQ==";
 
-        System.out.println(healthPayUtil.httpRquestBodyToJbfPay(data, funcId, privateKey, developerId, desKey));
+        // System.out.println(healthPayUtil.httpRquestBodyToJbfPay(data, funcId, privateKey, developerId, desKey));
 
-        String retj ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        String retj = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "<root>\n" +
                 "    <head>\n" +
                 "        <developer_id>59027</developer_id>\n" +
@@ -166,7 +149,9 @@ public class healthPayUtil {
                 "    </body>\n" +
                 "</root>";
 
-       // System.out.println(healthPayUtil.httpResponseBodyFromJbfPay(retj,desKey ,funcId));
+        System.out.println("healthPayUtil.httpResponseBodyFromJbfPay(retj,publicKey,desKey,funcId) = " + healthPayUtil.httpResponseBodyFromJbfPay(retj, publicKey, desKey, funcId));
+
+        // System.out.println(healthPayUtil.httpResponseBodyFromJbfPay(retj,desKey ,funcId));
 //        JSONObject jsonObject = new JSONObject();
 //        P2005RetDTO p2005RetDTO = new P2005RetDTO();
 //        p2005RetDTO.setOrderNo("ddd");
